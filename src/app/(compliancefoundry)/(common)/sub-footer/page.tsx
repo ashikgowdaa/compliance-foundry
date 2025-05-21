4109d02c84ea445001f0a7c55aa7da34e698924b
ashikgowdaa <ashik.ananda12@gmail.com>
2025-05-21 16:18:43 +0530

    Final Changes

--- src/app/(compliancefoundry)/(common)/sub-footer/page.tsx
+++ src/app/(compliancefoundry)/(common)/sub-footer/page.tsx
@@ -1,14 +1,18 @@
 import { getMethod } from "@/app/utils-api/getMethod";
 import Header from "@/components/Header";
 
-export default async function HeaderSection() {
+export default async function SubHeaderSection() {
   const data = await getMethod({
     url: "header-websites?populate[headerData][populate]=*",
     options: {
       cache: "force-cache", 
     },
   });
+  const header = data?.data?.[0]?.headerData;
 
+  if (!header) {
+    return <div>Header data not found</div>; // Or render nothing / fallback component
+  }
 
-  return <Header headerData={data.data[0].headerData} />;
+  return <Header headerData={header} />;
 }
